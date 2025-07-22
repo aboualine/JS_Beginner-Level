267b9494b48797a90b9b52a673f452f35c8ff56e
ABOUALINE MOHAMED <mohamedaboualine@gmail.com>
2025-07-22 20:40:04 +0100

    second commit on the ToDoList creating the delete button

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -1,30 +1,94 @@
-let tasks = [];
-document.getElementById("btnAdd").addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("button clicked seccusfully");
+// const { createElement } = require("react");
 
-    let content = document.getElementById("taskName").value;
+// let tasks = [];
+// document.getElementById("btnAdd").addEventListener("click", (event) => {
+//     event.preventDefault();
+//     console.log("button clicked seccusfully");
+
+//     let content = document.getElementById("taskName").value;
     
-    if (content.trim() === "") {
-        console.log("Empty input not added.");
-        return;
-    }
+//     if (content.trim() === "") {
+//         console.log("Empty input not added.");
+//         return;
+//     }
 
-    tasks.push(content);
+//     tasks.push(content);
 
-    document.getElementById("taskNameAdded").innerHTML = "";
+//     // document.getElementById("taskNameAdded").innerHTML = "";
 
-    for (let x = 0; x < tasks.length; x++) {
-        let element = tasks[x];
-        document.getElementById("taskNameAdded").innerHTML += "<div class ='divChoix'><input type ='radio'  class='tasksChoix'>"+element+"</input><button class ='rv'>delete</button></div><br>";
+//     // for (let x = 0; x < tasks.length; x++) {
+//     //     let element = tasks[x];
+//     //     document.getElementById("taskNameAdded").innerHTML += "<div class ='divChoix'><input type ='radio'  class='tasksChoix'>"+element+"</input><button class ='rv'>delete</button></div><br>";
         
+//     // }
+    
+//     // for(let x = 0; x < tasks.length; x++){
+//         let childDiv = createElement("div");
+//         let parentDiv = document.getElementById("taskNameAdded");
+//         let inputText = createElement("input[type='radio']");
+//         let deleteBtn = createElement("button");
+//         inputText.textContent = content;
+//         deleteBtn.textContent = "delete";
+//         childDiv.appendChild(inputText);
+//         childDiv.appendChild(deleteBtn);
+//         parentDiv.appendChild(childDiv);
+
+//         console.log(childDiv);
+        
+//     // }
+
+//     console.log(tasks);
+// });
+// // document.getElementById("button").addEventListener("click" , () => {
+// //     // let deletedElement = document.getElementById("tasksChoix").value;
+// //     // console.log("task : " + deletedElement);
+// //     // delete deletedElement;
+// //     // let div = document.getElementById("divChoix");
+// //     // delete div;
+// //     console.log("buuton clicked...")
+// // });
+let table = [];
+
+document.getElementById("btnAdd").addEventListener("click" , (event) => {
+    event.preventDefault();
+    let inputContent = document.getElementById("taskName").value;
+    if (inputContent === "") {
+        window.alert("you can't enter a null value");
     }
-    console.log(tasks);
-});
-document.getElementsByClassName("rv").addEventListener("click" , () => {
-    let deletedElement = document.getElementById("tasksChoix").value;
-    console.log("task : " + deletedElement);
-    delete deletedElement;
-    let div = document.getElementById("divChoix");
-    delete div;
+    else {
+        console.log("btn clicked!!!!");
+        table.push(inputContent);
+        let parentDiv = document.getElementById("taskNameAdded");
+        let divToAdd = document.createElement("div");
+        let radioBtn = document.createElement("input");
+        radioBtn.setAttribute("type", "radio");
+        radioBtn.setAttribute("value",inputContent);
+        radioBtn.setAttribute("name","task");
+        let label = document.createElement("label");
+        label.textContent = inputContent;
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "X";
+        deleteBtn.setAttribute("id", "delete");
+        deleteBtn.setAttribute("data-value", inputContent);
+        divToAdd.appendChild(radioBtn);
+        divToAdd.appendChild(label);
+        divToAdd.appendChild(deleteBtn);
+        parentDiv.appendChild(divToAdd);
+        console.log("value added is : "+inputContent);
+        console.log(table);
+        document.getElementById("delete").addEventListener("click" , (event) => {
+            console.log("btn delete clicked");
+            const button = event.target;
+            const taskToDelete = button.getAttribute("data-value");
+            const taskIndex = table.indexOf(taskToDelete);
+            if (taskIndex !== -1) {
+                table.splice(taskIndex, 1);
+                console.log("the task removed is : "+taskToDelete);
+            }
+            const parentDiv = button.parentElement;
+            parentDiv.remove();
+            console.log("the new table is : "+table);
+        });
+    }
+    
 });
